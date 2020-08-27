94f6a661cb7cfd43eef13c30f4e31de320ae32e7
Artur Teben <teben.artur99@gmail.com>
2020-08-27 20:30:18 +0300

    Create entity Book

---
+++ src/main/java/com/teben/models/Book.java
@@ -0,0 +1,34 @@
+package com.teben.models;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Setter
+@Getter
+@EqualsAndHashCode
+@ToString
+@NoArgsConstructor
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "author")
+    private String author;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
+    private User owner;
+
+    public Book(String name, String author) {
+        this.name = name;
+        this.author = author;
+    }
+}
+
